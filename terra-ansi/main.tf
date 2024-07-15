a8e62c1b0f4b57bc9aa42ec28579d65f16e0ee17
Prajjwal <prajjwalpawar1@gmail.com>
2024-07-15 15:38:09 +0530

    craeted an ec2 instance

--- terra-ansi/main.tf
+++ terra-ansi/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.58.0"
     }
   }
 }
@@ -11,3 +11,28 @@ provider "aws" {
   region = var.AWS_REGION
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.INSTANCE_TYPE
+  key_name      = "New-key"
+
+  tags = {
+    Name = "Streaming frontend"
+  }
+}
--- terra-ansi/variables.tf
+++ terra-ansi/variables.tf
@@ -2,3 +2,8 @@ variable "AWS_REGION" {
   description = "Region for aws resources"
   default     = "us-east-1"
 }
+
+variable "INSTANCE_TYPE" {
+  description = "Type of instance to be used"
+  default     = "t3.micro"
+}
