36a39b91359fedbd3e04c6b82645b7eb440c9017
Prajjwal <prajjwalpawar1@gmail.com>
2024-07-17 11:08:07 +0530

    new instance added

--- Terraform/modules/ec2/main.tf
+++ Terraform/modules/ec2/main.tf
@@ -1,7 +1,23 @@
-resource "aws_instance" "EC2" {
-  count = 2
-  ami = var.AMI
-  instance_type = var.INSTANCE_TYPE
-  subnet_id = var.SUBNET_ID
-  security_groups = var.SECURITY_GROUPS
+resource "aws_instance" "frontend-ec2" {
+  count           = 2
+  ami             = var.AMI
+  instance_type   = var.INSTANCE_TYPE
+  subnet_id       = [var.SUBNET_ID]
+  security_groups = [var.SECURITY_GROUPS]
+
+  tags = {
+    Name = "frontend-ec2-${count.index}"
+  }
+}
+
+resource "aws_instance" "backend-ec2" {
+  count           = 2
+  ami             = var.AMI
+  instance_type   = var.INSTANCE_TYPE
+  subnet_id       = [var.SUBNET_ID]
+  security_groups = [var.SECURITY_GROUPS]
+
+  tags = {
+    Name = "backend-ec2-${count.index}"
+  }
 }
