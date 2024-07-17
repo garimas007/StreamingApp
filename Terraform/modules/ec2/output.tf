9f3a67f9ec10b74efd48e1a7607b62b8d9bdb58b
Prajjwal <prajjwalpawar1@gmail.com>
2024-07-17 11:09:30 +0530

    new changes made

--- Terraform/modules/ec2/output.tf
+++ Terraform/modules/ec2/output.tf
@@ -1,7 +1,7 @@
 output "frontend_instance_id" {
-  value = aws_instance.frontend-ec2.id
+  value = aws_instance.frontend-ec2[*].id
 }
 
 output "backend_instance_id" {
-  value = aws_instance.backend-ec2.id
+  value = aws_instance.backend-ec2[*].id
 }
